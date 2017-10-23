a256145e056f9e0473783e005b57169683396d48
Vladimír Fojtík <fojtik.v@gmail.com>
2017-10-23 18:11:58 +0200

    Added chart.js

--- resources/assets/js/components/App.js
+++ resources/assets/js/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Chart from './Chart';
 
 export default class App extends Component {
     render() {
@@ -11,7 +12,7 @@ export default class App extends Component {
                             <div className="panel-heading">Example Component</div>
 
                             <div className="panel-body">
-                                I'm an example component!
+                                <Chart />
                             </div>
                         </div>
                     </div>
