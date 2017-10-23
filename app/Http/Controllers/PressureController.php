160c59f9a3cdb09a85a2cea60bae6434736c53d4
Vladimír Fojtík <fojtik.v@gmail.com>
2017-10-23 18:05:32 +0200

    Added pressure api point

---
+++ app/Http/Controllers/PressureController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PressureController extends Controller
+{
+    public function data()
+    {
+        return \App\Pressure::limit(1000)->get();
+    }
+}
