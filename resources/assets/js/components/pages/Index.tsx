c0ee23098be493f016e4c3a852c30add3a2ee222
Vladimír Fojtík <fojtik.v@gmail.com>
2017-10-28 16:35:32 +0200

    added more charts for testing purpose

--- resources/assets/js/components/pages/Index.tsx
+++ resources/assets/js/components/pages/Index.tsx
@@ -1,18 +1,41 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import Chart from '../Chart';
+import PressureChart from '../charts/PressureChart';
+import TemperatureChart from '../charts/TemperatureChart';
+import WindSpeedChart from '../charts/WindSpeedChart';
 
 export default class App extends React.Component {
     render() {
         return (
             <div className="container">
                 <div className="row">
-                    <div className="col-md-8 col-md-offset-2">
+                    
+                    <div className="col-md-6">
                         <div className="panel panel-default">
-                            <div className="panel-heading">Index Component</div>
+                            <div className="panel-heading">Tlak</div>
 
                             <div className="panel-body">
-                                <Chart />
+                                <PressureChart />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-md-6">
+                        <div className="panel panel-default">
+                            <div className="panel-heading">Teplota</div>
+
+                            <div className="panel-body">
+                                <TemperatureChart />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-md-6">
+                        <div className="panel panel-default">
+                            <div className="panel-heading">Rychlost větru</div>
+
+                            <div className="panel-body">
+                                <WindSpeedChart />
                             </div>
                         </div>
                     </div>
