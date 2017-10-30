93ec04ffb35b668a9fec9d0107601880a96f1cff
Lukáš Antl <P17075@student.osu.cz>
2017-10-30 13:15:43 +0100

    edit temperatures api point for picking by start date and end date

--- app/Http/Controllers/TemperatureController.php
+++ app/Http/Controllers/TemperatureController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Temperature;
 
 class TemperatureController extends Controller
 {
     public function index()
     {
-        return \App\Temperature::limit(1000)->get();
+        $result;
+
+        if (request()->query('start_date') != '' && request()->query('end_date') != '') {
+            $result = Temperature::where('date', '>=', request()->query('start_date'))
+                ->where('date', '<=', request()->query('end_date'))
+                ->get();
+        } else {
+            $result = Temperature::limit(100)->get();
+        }
+
+        return $result;
+
     }
 }
