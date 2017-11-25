a07e580e214a1e30f2624812a58db2ab4f025611
Vladimír Fojtík <fojtik.v@gmail.com>
2017-11-25 08:34:54 +0100

    little fix for database seeders

--- database/seeds/WindsTableSeeder.php
+++ database/seeds/WindsTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class WindsTableSeeder extends Seeder
@@ -11,6 +12,26 @@ class WindsTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Wind::class, 50)->create();
+        $dateTime = Carbon::now();
+        $faker = Faker\Factory::create();
+        $speed = $faker->numberBetween(5, 50);
+
+        for ($i = 0; $i < 1000; $i++) {
+            if (random_int(1, 6) <= 3) {
+                $max = ($speed + 10 <= 100) ? $speed + 10 : 100;
+                $speed = $faker->numberBetween($speed, $max);
+            } else {
+                $min = ($speed - 10 >= 5) ? $speed - 10 : 5;
+                $speed = $faker->numberBetween($min, $speed);
+            }
+
+            $wind = factory(App\Wind::class)->make([
+                'date' => $dateTime,
+                'speed' => $speed
+            ]);
+
+            $wind->save();
+            $dateTime->addMinutes(2);
+        }
     }
 }
