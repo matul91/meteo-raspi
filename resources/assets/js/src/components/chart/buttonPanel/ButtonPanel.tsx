ab34011fbe800a39ace13013f868215caf776451
Vladimír Fojtík <fojtik.v@gmail.com>
2018-03-17 18:54:25 +0100

    added ButtonGroup to wrap buttons in ButtonPanel

--- resources/assets/js/src/components/chart/buttonPanel/ButtonPanel.tsx
+++ resources/assets/js/src/components/chart/buttonPanel/ButtonPanel.tsx
@@ -1,23 +1,25 @@
 import * as React from "react";
-import {Button} from "react-bootstrap";
+import {Button, ButtonGroup} from "react-bootstrap";
 import * as Directions from "../../../config/constants/directions";
 
 const ButtonPanel = (props) => {
     return (
         <div className="text-right chart-buttons">
-            <Button
-                name={Directions.MINUS}
-                onClick={props.clickHandler}
-            >
-                Předchozí
-            </Button>
-            <Button
-                name={Directions.PLUS}
-                onClick={props.clickHandler}
-                disabled={props.initialDate === props.lastDate}
-            >
-                Další
-            </Button>
+            <ButtonGroup>
+                <Button
+                    name={Directions.MINUS}
+                    onClick={props.clickHandler}
+                >
+                    Předchozí
+                </Button>
+                <Button
+                    name={Directions.PLUS}
+                    onClick={props.clickHandler}
+                    disabled={props.initialDate === props.lastDate}
+                >
+                    Další
+                </Button>
+            </ButtonGroup>
         </div>
     );
 };
