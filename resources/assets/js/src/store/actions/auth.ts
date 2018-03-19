206497447ae8dc608e7bc09a37a6d7df1c3578b6
Vladimír Fojtík <fojtik.v@gmail.com>
2018-03-19 07:54:30 +0100

    fix - set correct expirationTime in redux auth action

--- resources/assets/js/src/store/actions/auth.ts
+++ resources/assets/js/src/store/actions/auth.ts
@@ -55,7 +55,7 @@ export const auth = (email, password) => {
         axios.post(process.env.MIX_OAUTH_ADD, data)
             .then((response) => {
                 const accessToken = response.data.access_token;
-                const expirationDate = new Date(new Date().getTime() + response.data.expires_in * 1000);
+                const expirationDate = new Date(new Date().getTime() + response.data.expires_in * 1000 * 60);
                 const headerData = { Authorization: `Bearer ${accessToken}` };
                 dispatch(authGetUserInfo(headerData, accessToken, expirationDate));
             })
