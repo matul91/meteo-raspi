85c5da9a3b73c1184beda4a92ca28cea97e6738c
Vladimír Fojtík <fojtik.v@gmail.com>
2018-03-30 14:58:56 +0200

    Removed wind direction component from current photo component

--- resources/assets/js/src/components/currentPhoto/CurrentPhoto.tsx
+++ resources/assets/js/src/components/currentPhoto/CurrentPhoto.tsx
@@ -1,15 +1,13 @@
 import * as React from "react";
 import {Panel} from "react-bootstrap";
-import WindDirection from "../windDirection/WindDirection";
 
 const CurrentPhoto = (props) => {
     return (
         <Panel className="current-photo">
             <Panel.Heading>
-                <Panel.Title>Current photo and wind direction marker</Panel.Title>
+                <Panel.Title>Current photo</Panel.Title>
             </Panel.Heading>
             <Panel.Body>
-                <WindDirection direction={props.direction} speed={props.speed} />
                 <img src="http://via.placeholder.com/1920x1080" alt="Current photo" className="img-responsive" />
             </Panel.Body>
         </Panel>
