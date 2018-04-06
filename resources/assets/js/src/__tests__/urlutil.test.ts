4962b668c707aa805a61552e6b181cca1a36b4ab
Vladimír Fojtík <fojtik.v@gmail.com>
2018-04-06 16:07:43 +0200

    Reduced duplicities, try no. 1

--- resources/assets/js/src/__tests__/urlutil.test.ts
+++ resources/assets/js/src/__tests__/urlutil.test.ts
@@ -4,17 +4,14 @@ import URLUtil from "../utils/URLUtil";
 describe("URLUtil", () => {
     it("check if URL address is generated in correct format", () => {
         const generatedURL = URLUtil.generateURLByPosition("pressures", "latest");
-        const expectedURL = "pressures/latest";
-        expect(generatedURL).toBe(expectedURL);
+        expect(generatedURL).toBe("pressures/latest");
     });
     it("check if it's returned original URL if one of dates is null", () => {
         const generatedURL = URLUtil.generateURLByDates("pressures", "2018-03-03");
-        const expectedURL = "pressures";
-        expect(generatedURL).toBe(expectedURL);
+        expect(generatedURL).toBe("pressures");
     });
     it("check if it's returned URL with start_date and end_date params", () => {
         const generatedURL = URLUtil.generateURLByDates("pressures", "2018-03-03", "2018-03-04");
-        const expectedURL = "pressures/?start_date=2018-03-03&end_date=2018-03-04";
-        expect(generatedURL).toBe(expectedURL);
+        expect(generatedURL).toBe("pressures/?start_date=2018-03-03&end_date=2018-03-04");
     });
 });
