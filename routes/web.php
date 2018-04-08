6390a1e171c62a955fbf7b07879a121cc005edf3
Lukáš Antl <lukes.antl@seznam.cz>
2018-04-08 22:50:42 +0200

    Created new security for photo api point

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/winds/latest', 'WindController@latest');
 
 Route::get('/photo', 'PhotoController@index');
 Route::get('/photo/all', 'PhotoController@all');
-Route::post('/photo/save', 'PhotoController@savePhoto');
+Route::post('/photo/save', 'PhotoController@savePhoto')->middleware('chekweatherstation');
 
 Route::any('{all}', function () {
     return view('index');
