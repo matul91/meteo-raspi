03cfac8f9dfd83bc2641e200160f4e2a5bbfa88e
Lukáš Antl <lukes.antl@seznam.cz>
2018-04-10 13:15:46 +0200

    Added tests for photo API point

---
+++ tests/Browser/PhotoApiPointTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+
+class PhotoApiPointTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+
+    public function testUnauthorized()
+    {
+        $response = $this->post('/photo/save', ['body' => ['metaInfo' => 'people']], ['Authorization' => '']);
+        $response->assertStatus(401);
+    }
+
+    public function testSendEmptyImage()
+    {
+        $response = $this->post('/photo/save', ['body' => ['metaInfo' => 'people']], ['Authorization' => env('PASSWORD_WEATHER_STATION')]);
+        $response->assertStatus(500);
+    }
+
+    public function testShowPageLastPhoto()
+    {
+        $response = $this->get('/photo');
+        $response->assertStatus(200);
+    }
+
+    public function testShowPageAllPhotos()
+    {
+        $response = $this->get('/photo/all');
+        $response->assertStatus(200);
+    }
+
+}
