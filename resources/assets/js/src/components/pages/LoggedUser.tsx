1c9539cc2f036bdeecc697038ea2fdf3afdba031
Vladimír Fojtík <fojtik.v@gmail.com>
2018-04-11 16:58:19 +0200

    Added support for PWAs in Laravel and created basic structure to communicate with FCM

--- resources/assets/js/src/components/pages/LoggedUser.tsx
+++ resources/assets/js/src/components/pages/LoggedUser.tsx
@@ -1,23 +1,47 @@
+import * as firebase from "firebase";
 import * as React from "react";
 import {Col, PageHeader, Row} from "react-bootstrap";
-import CurrentPhoto from "../currentPhoto/CurrentPhoto";
 import PhotosHistory from "../photosHistory/PhotosHistory";
 
-const LoggedUser = () => {
-    return (
-        <Row>
-            <Col xs={12}>
-                <PageHeader>
-                    User's page
-                </PageHeader>
-                <Row>
-                    <Col sm={12}>
-                        <PhotosHistory />
-                    </Col>
-                </Row>
-            </Col>
-        </Row>
-    );
-};
+class LoggedUser extends React.Component {
+    public componentDidMount(): void {
+        const config = {
+            apiKey: process.env.MIX_FIREBASE_API_KEY,
+            authDomain: process.env.MIX_FIREBASE_AUTH_DOMAIN,
+            databaseURL: process.env.MIX_FIREBASE_DATABASE_URL,
+            messagingSenderId: process.env.MIX_FIREBASE_MESSAGING_SENDER_ID,
+            projectId: process.env.MIX_FIREBASE_PROJECT_ID,
+            storageBucket: process.env.MIX_FIREBASE_STORAGE_BUCKET,
+        };
+        firebase.initializeApp(config);
+        const messaging = firebase.messaging();
+        messaging.usePublicVapidKey(
+            "BGBPRvOncmbGITMU8cgt7b-g4q9fQjXC8eUAueCj0kGe4D3gs1JcyBbsrYqecIGig8kP1sbhnqWva-u29gzULd8"
+        );
+        messaging.requestPermission()
+            .then(() => messaging.getToken())
+            .then((token) => {
+                console.log("Granted", token);
+            })
+            .catch((err) => console.log("Denied", err));
+    }
+
+    public render(): JSX.Element {
+        return (
+            <Row>
+                <Col xs={12}>
+                    <PageHeader>
+                        User's page
+                    </PageHeader>
+                    <Row>
+                        <Col sm={12}>
+                            <PhotosHistory/>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        );
+    }
+}
 
 export default LoggedUser;
