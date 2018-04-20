a47b56d7f5f263040f47e47482cbc15796478b98
Vladimír Fojtík <fojtik.v@gmail.com>
2018-04-20 10:46:21 +0200

    Added tests for downSampleArray function

--- resources/assets/js/src/__tests__/arrayutil.test.ts
+++ resources/assets/js/src/__tests__/arrayutil.test.ts
@@ -1,4 +1,5 @@
 import * as Directions from "config/constants/directions";
+import * as Values from "config/constants/values";
 import {} from "jest";
 import ArrayUtil from "utils/ArrayUtil";
 
@@ -21,5 +22,21 @@ describe("ArrayUtil", () => {
         const secondArray = ["b"];
         const destructuredArray = ArrayUtil.destructureDataArrays(Directions.PLUS, firstArray, secondArray);
         expect(destructuredArray).toEqual(["a", "b"]);
+    });
+    it("check if downSampled array that has less elements than NUMBER_OF_SAMPLES constant has original length", () => {
+        const testArray = [];
+        for (let i = 0; i < 20; i++) {
+            testArray.push({ date: "2018-03-25", value: i });
+        }
+        const downSampledArray = ArrayUtil.downSampleArray(testArray);
+        expect(downSampledArray.length).toBe(testArray.length);
+    });
+    it("check if downSampled array has length given by NUMBER_OF_SAMPLES constant", () => {
+        const testArray = [];
+        for (let i = 0; i < 50; i++) {
+            testArray.push({ date: "2018-03-25", value: i });
+        }
+        const downSampledArray = ArrayUtil.downSampleArray(testArray);
+        expect(downSampledArray.length).toBe(Values.NUMBER_OF_SAMPLES);
     });
 });
