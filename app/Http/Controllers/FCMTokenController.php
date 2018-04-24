a5b4baf7fcdadba35e8a3a4e5f2e3d6130541161
Lukáš Antl <lukes.antl@seznam.cz>
2018-04-24 16:31:43 +0200

    renamed token to FCMToken and deleted unused index in FCMTokenController

--- app/Http/Controllers/FCMTokenController.php
+++ app/Http/Controllers/FCMTokenController.php
@@ -10,19 +10,15 @@ class FCMTokenController extends Controller
 {
     public function addToken(Request $request)
     {
-        if ($request->has('token')) {
-            $token = new Token();
-            $token->user_id = User::getUserIdFromToken($request);
-            $token->token = $request->token;
-            $token->save();
+        if ($request->has('FCMToken')) {
+            $FCMToken = new Token();
+            $FCMToken->user_id = User::getUserIdFromToken($request);
+            $FCMToken->token = $request->FCMToken;
+            $FCMToken->save();
             return response()->json(["proved" => true, "message" => "Token has been added"], 200);
         } else {
             return response()->json(["proved" => false, "message" => "Token is empty"], 500);
         }
     }
-
-    public function index()
-    {
-        return "Test";
-    }
+    
 }
