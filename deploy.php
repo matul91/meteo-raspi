98aeb41cde3f5899e8f0e4e8ab1900ec571507ab
Jiri Matula <matula.jirik@gmail.com>
2018-05-11 18:32:01 +0200

    Set writable mode chmod

--- deploy.php
+++ deploy.php
@@ -11,7 +11,7 @@ set('application', 'meteo-raspi');
 // Configuration
 set('ssh_type', 'native');
 set('ssh_multiplexing', true);
-set('writable_mode', 'chgrp');
+set('writable_mode', 'chmod');
 set('repository', 'https://github.com/matul91/meteo-raspi.git');
 set('branch', 'master');
 set('keep_releases', 5);
