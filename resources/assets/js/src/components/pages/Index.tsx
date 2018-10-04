f7731e344cf3b3f4b6b86f7057b211f6cd274406
Jiri Matula <matula.jirik@gmail.com>
2018-10-04 21:27:58 +0200

    fixup! Last photos component

--- resources/assets/js/src/components/pages/Index.tsx
+++ resources/assets/js/src/components/pages/Index.tsx
@@ -1,9 +1,8 @@
-import Chart from "components/chart/Chart";
 import Loading from "components/loading/Loading";
-import Icon from "components/svgIcon/SvgIcon";
 import * as React from "react";
 import { connect } from "react-redux";
 import { Alert, Col, Media, Row } from "reactstrap";
+import PhotosHistory from "../photosHistory/PhotosHistory";
 
 interface IProps {
     dataSets: any;
@@ -14,62 +13,18 @@ interface IProps {
 class Index extends React.Component<IProps, void> {
     public render(): JSX.Element {
         let content = <Loading text="Loading..."/>;
-        const charts = this.getCharts();
         if (this.props.isLoaded) {
             content = (
                 <Row className="no-gutters">
                     <Col xs={3}>
-                        <Alert color="primary">
-                            <div className="d-flex">
-                                <div className="p-2">
-                                    <span>Poslední <br/> snímky</span>
-                                </div>
-                                <div className="p-2 ml-auto align-self-center" >
-                                    <Icon kind="history" />
-                                </div>
-                            </div>
-                        </Alert>
-                        <div>
-                            <div className="d-flex justify-content-end mb-4">
-                                <img className="img-fluid img-snapshot" src="/images/camera-pic.png" />
-                            </div>
-                            <div className="d-flex justify-content-end mb-4">
-                                <img className="img-fluid img-snapshot" src="/images/camera-pic.png" />
-                            </div>
-                            <div className="d-flex justify-content-end mb-4">
-                                <img className="img-fluid img-snapshot" src="/images/camera-pic.png" />
-                            </div>
-                        </div>
+                        <PhotosHistory/>
                     </Col>
-                    <Col xs={6} />
-                    <Col xs={3}>
-                    </Col>
+                    <Col xs={6}/>
+                    <Col xs={3}/>
                 </Row>
             );
         }
         return content;
-    }
-
-    private getCharts(): JSX.Element[] {
-        return Object.keys(this.props.dataSets).map((setName) => {
-            return (
-                <Col md={6} className="mb-4" key={this.props.dataSets[setName].columnName}>
-                    <Chart
-                        setName={setName}
-                        data={this.props.dataSets[setName].data}
-                        dataMeta={this.props.dataSets[setName].dataMeta}
-                        initialDate={this.props.dataSets[setName].initialDate}
-                        initialValue={this.props.dataSets[setName].initialValue}
-                        name={this.props.dataSets[setName].name}
-                        url={this.props.dataSets[setName].url}
-                        columnName={this.props.dataSets[setName].columnName}
-                        suffix={this.props.dataSets[setName].suffix}
-                        error={this.props.dataSets[setName].error}
-                        loading={this.props.dataSets[setName].loading}
-                    />
-                </Col>
-            );
-        });
     }
 }
 
