4890c995688abdf5e704b1bcc0b73126fb80b5c0
Jiří Matula <matula.jirik@gmail.com>
2018-10-05 13:54:12 +0200

    Add alert component (#58)
    * Add alert component
    
    * fixup! Add alert component

--- resources/assets/js/src/components/pages/Index.tsx
+++ resources/assets/js/src/components/pages/Index.tsx
@@ -1,10 +1,8 @@
-import AerialPhoto from "components/aerialPhoto/AerialPhoto";
-import Chart from "components/chart/Chart";
-import CurrentPhoto from "components/currentPhoto/CurrentPhoto";
 import Loading from "components/loading/Loading";
+import StatusAlerts from "components/statusAlerts/StatusAlerts";
 import * as React from "react";
-import { Col, Row } from "react-bootstrap";
 import { connect } from "react-redux";
+import { Alert, Col, Media, Row } from "reactstrap";
 
 interface IProps {
     dataSets: any;
@@ -15,35 +13,18 @@ interface IProps {
 class Index extends React.Component<IProps, void> {
     public render(): JSX.Element {
         let content = <Loading text="Loading..."/>;
-        const charts = this.getCharts();
         if (this.props.isLoaded) {
             content = (
-                <div/>
+                <Row className="no-gutters">
+                    <Col xs={3}/>
+                    <Col xs={6}/>
+                    <Col xs={3}>
+                        <StatusAlerts/>
+                    </Col>
+                </Row>
             );
         }
         return content;
-    }
-
-    private getCharts(): JSX.Element[] {
-        return Object.keys(this.props.dataSets).map((setName) => {
-            return (
-                <Col md={6} className="mb-4" key={this.props.dataSets[setName].columnName}>
-                    <Chart
-                        setName={setName}
-                        data={this.props.dataSets[setName].data}
-                        dataMeta={this.props.dataSets[setName].dataMeta}
-                        initialDate={this.props.dataSets[setName].initialDate}
-                        initialValue={this.props.dataSets[setName].initialValue}
-                        name={this.props.dataSets[setName].name}
-                        url={this.props.dataSets[setName].url}
-                        columnName={this.props.dataSets[setName].columnName}
-                        suffix={this.props.dataSets[setName].suffix}
-                        error={this.props.dataSets[setName].error}
-                        loading={this.props.dataSets[setName].loading}
-                    />
-                </Col>
-            );
-        });
     }
 }
 
