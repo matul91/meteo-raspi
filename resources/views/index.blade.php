8e8594888a542fc9b164eee8c290253b5c299182
Jiri Matula <jiri.matula@lmc.eu>
2019-11-13 07:36:10 +0100

    Enable test progressive web app definitions

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="theme-color" content="#5A84E5"/>
 
         <title>{{ config('app.name', 'Meteo RasPi') }}</title>
 
