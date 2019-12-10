a059587447b6b949a7643022516a55e8ade916e1
Jiri Matula <jiri.matula@lmc.eu>
2019-12-10 19:15:56 +0100

    Minor changes to improve usability of application

--- resources/assets/js/src/pages/Main.tsx
+++ resources/assets/js/src/pages/Main.tsx
@@ -1,22 +1,17 @@
 import PhotosHistory from "components/photosHistory/PhotosHistory";
 import * as React from "react";
-import { connect } from "react-redux";
-import { Alert, Col, Media, Row } from "reactstrap";
-import StatusAlertsExample from "../components/statusAlerts/StatusAlertsExample";
+import {Col, Row } from "reactstrap";
 import WindDirection from "../components/windDirection/WindDirection";
 
 export default class Main extends React.PureComponent {
     public render(): JSX.Element {
         return  (
             <Row className="no-gutters">
-                <Col xs={3}>
+                <Col xs={10} md={3}>
                     <PhotosHistory/>
                 </Col>
-                <Col xs={6}>
+                <Col xs={11} md={9}>
                     <WindDirection/>
-                </Col>
-                <Col xs={3}>
-                    <StatusAlertsExample />
                 </Col>
             </Row>
         );
